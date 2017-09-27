e7be9afdfdd969f16ab7ad761654487413c82780
Brian McMinn <brianmsj@gmail.com>
2017-09-27 16:02:44 -0700

    finished directives for submit

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 angular.module('myApp',[])
-.controller('main', function() {
+.controller('main', function($scope) {
   var vm = this
   vm.price = 0;
   vm.taxrate = 0;
-  vm.perentage = 0;
+  vm.percentage = 0;
+  vm.firstSubtotal = 0;
+
+  vm.subtotal = function() {
+    var totalPercentage = vm.taxrate / 100 + 1
+    vm.firstSubtotal = vm.price * totalPercentage
+    vm.firstSubtotal = vm.firstSubtotal.toFixed(2)
+  }
+
+$scope.myForms = {};
+    $scope.submit = function() {
+        if($scope.myForms.meal.$valid) {
+          console.log('fired')
+          vm.subtotal()
+        }
+        else {
+          alert('form has errors')
+        }
+}
+
 
 })
