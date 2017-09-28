9e8c4eea98deaea3b17a845e12743a6d003a9c9b
Brian McMinn <brianmsj@gmail.com>
2017-09-27 21:19:07 -0700

    completed all calculations

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ angular.module('myApp',[])
   vm.firstSubtotal = 0;
   vm.tiptotal = 0;
   vm.totalbill = 0;
+  vm.customercount = 0;
+  vm.totaltipsarray = []
+  vm.totaltips = 0;
+  vm.averagetips = 0;
 
   vm.subtotal = function() {
     var totalPercentage = vm.taxrate / 100 + 1;
@@ -19,10 +23,27 @@ angular.module('myApp',[])
     var parsed = parseInt(vm.firstSubtotal)
     vm.tiptotal = vm.tiptotal - parsed
     vm.tiptotal = vm.tiptotal.toFixed(2)
+    vm.totaltipsarray.push(vm.tiptotal)
   }
   vm.total = function() {
     vm.totalbill = parseInt(vm.firstSubtotal) + parseInt(vm.tiptotal)
     vm.totalbill = vm.totalbill.toFixed(2)
+  }
+  vm.addtips = function() {
+    var results = []
+    var totalaverage = 0
+    for(let i=0;i<vm.totaltipsarray.length;i++) {
+      var parsing = parseFloat(vm.totaltipsarray[i])
+      results.push(parsing)
+    }
+    for(let j=0;j<results.length;j++) {
+       totalaverage += results[j]
+    }
+    console.log(results)
+    vm.totaltips = results.reduce((a, b) => a + b, 0).toFixed(2);
+    vm.averagetips = totalaverage / results.length
+    vm.averagetips = vm.averagetips.toFixed(2)
+    console.log(vm.totaltips)
   }
 
 $scope.myForms = {};
@@ -32,6 +53,8 @@ $scope.myForms = {};
           vm.subtotal()
           vm.tip()
           vm.total()
+          vm.customercount++
+          vm.addtips();
         }
         else {
           alert('form has errors')
